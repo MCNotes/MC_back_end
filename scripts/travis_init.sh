85ea8a23b5d6bac33b84a17b176cc5f6ea063c0f
Tania Allard <t.allard@sheffield.ac.uk>
2017-07-28 15:53:58 +0100

    update travis for PR and non PR builds

--- scripts/travis_init.sh
+++ scripts/travis_init.sh
@@ -1,3 +1,12 @@
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then bash run_pull_requests.sh
 
-elif ["$TRAVIS_PULL_REQUEST" == "false"]; then bash run_merge.sh
+# Checks firs if this was or not a pull request and calls the
+# approrpiate scripts
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then bash run_pull_requests.sh
+echo "Running on Pull Request"
+
+elif ["$TRAVIS_PULL_REQUEST" == "false"]
+then bash run_merge.sh
+echo "Merged pull request found"
+
+fi
