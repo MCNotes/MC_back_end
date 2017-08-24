38da6457d218739c3be5555fd58064a724fd6914
Tania Allard <t.allard@sheffield.ac.uk>
2017-08-24 10:59:48 +0100

    Updated Travis initialize script

--- scripts/travis_init.sh
+++ scripts/travis_init.sh
@@ -1,12 +1,18 @@
+#!/bin/bash
+# Checks first if this was or not a pull request and calls the
+# appropriate scripts
 
-# Checks firs if this was or not a pull request and calls the
-# approrpiate scripts
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]
-then bash run_pull_requests.sh
-echo "Running on Pull Request"
+if [ "$TRAVIS_PULL_REQUEST" != "false" ];
+then
+  bash run_pull_requests.sh
+  echo "Running on Pull Request \n \n ";
 
-elif ["$TRAVIS_PULL_REQUEST" == "false"]
-then bash run_merge.sh
-echo "Merged pull request found"
+elif ["$TRAVIS_PULL_REQUEST" == "false"];
+then
+  bash run_merge.sh
+  echo "Merged pull request found";
 
-fi
+else
+  echo "Unknown event ocurred";
+
+fi;
